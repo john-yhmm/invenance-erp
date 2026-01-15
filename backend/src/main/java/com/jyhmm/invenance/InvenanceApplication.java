5b5d7a8bdf0f69e5278451891f93430d18039a06
JohnYHMM <johnyehtetmgmg@gmail.com>
2026-01-15 08:55:31 +0700

    Backend: Add project frame

---
+++ backend/src/main/java/com/jyhmm/invenance/InvenanceApplication.java
@@ -0,0 +1,13 @@
+package com.jyhmm.invenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvenanceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jyhmm/invenance/InvenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyhmm.invenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
